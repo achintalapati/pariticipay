4b2cba8172802df0e98ce41beecd42794e9abe65
EricLi <eric2927.li@gmail.com>
2021-01-23 14:57:01 -0500

    Added logo

--- src/Header.js
+++ src/Header.js
@@ -5,25 +5,35 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 import PersonIcon from '@material-ui/icons/Person';
 import { Link } from 'react-router-dom';
+import participay_logo from './participay_logo.png';
 
 function Header() {
   return (
     <div className="header">
-      <Link to='/leaderboard'>
-        <IconButton>
-          <BarChartIcon className="header__icon" fontSize="large"/>
-        </IconButton>
-      </Link>
-      <Link to='/'>
-        <IconButton>
-          <PersonIcon className="header__icon" fontSize="large"/>
-        </IconButton>
-      </Link>
-      <Link to='/store'>
-        <IconButton>
-          <ShoppingCartIcon className="header__icon" fontSize="large"/>
-        </IconButton>
-      </Link>
+      <div className="header_logo_box">
+        <img
+          className="header__logo"
+          src={participay_logo}
+          alt="logo"
+        />
+      </div>
+      <div className="header_buttons">
+        <Link to='/leaderboard'>
+          <IconButton>
+            <BarChartIcon className="header__icon" fontSize="large"/>
+          </IconButton>
+        </Link>
+        <Link to='/'>
+          <IconButton>
+            <PersonIcon className="header__icon" fontSize="large"/>
+          </IconButton>
+        </Link>
+        <Link to='/store'>
+          <IconButton>
+            <ShoppingCartIcon className="header__icon" fontSize="large"/>
+          </IconButton>
+        </Link>
+      </div>
     </div>
   )
 }
